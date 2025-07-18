f6d7f8c8251beeec5f0bcb6ed7c61c3e429714c8
Shubham Jadhav <76850951+shubhambhau@users.noreply.github.com>
2025-07-18 23:15:19 +0530

    Day-1 (Implementing GET service)
    Implementing a GET service to retrieve User Resources

---
+++ SpringBootFundamentals/src/main/java/in/shubhamjadhav/controller/StudentController.java
@@ -0,0 +1,39 @@
+package in.shubhamjadhav.controller;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import in.shubhamjadhav.beans.Student;
+
+@RestController
+public class StudentController {
+
+	public static List<Student> students = new ArrayList<Student>();
+	
+	
+	
+	public StudentController() {
+		students.add(new Student("shubham","Jadhav"));
+		students.add(new Student("Rushi","Solanke"));
+		students.add(new Student("Arjun","Mule"));
+		students.add(new Student("Dhanno","J"));
+		
+	}
+
+
+
+	@GetMapping("student")
+	public List<Student> getStudents(){
+		return students;
+	}
+	
+	@GetMapping("/student/{firstName}/{lastName}")
+	public Student studentPathVariable(@PathVariable("firstName") String firstName, @PathVariable("lastName") String lastName )
+	{
+		return new Student(firstName, lastName);
+	}
+}
